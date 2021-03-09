3ae8dd284df771414dc49075b9ee8b9c60092136
DOLBIMLED <50545147+Userteck@users.noreply.github.com>
2021-03-09 15:09:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,34 @@
 const search = document.getElementById('search-books');
 const bookList = document.getElementById('book-list');
 console.log(search, bookList);
-var titles = document.getElememtsByClassName('title');
+var titles = document.getElememtsByClassName('titles');
 
 
 console.log(Array.isArray(titles));
 console.log(Array.isArray(Array.from(titles)));
 
 
-titles.forEach(function(item)){
+titles.forEach(function(item){
     console.log(item);
 })
+
+
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+// console.log(wmf);
+
+var books = document.querySelector('#book-list li .name');
+// console.log(books);
+
+books = documents.querySelectorAll('#book-list li .name');
+// console.log(books);
+
+Array.from(books).forEach(function(book){
+    console.log(book);
+});
+
+
+var books = document.querySelectorAll('#book-list li .name');
+
+Array.from(books).forEach(function(book){
+    book.textContent = 'test';
+});
