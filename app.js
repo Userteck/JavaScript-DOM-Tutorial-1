dae65d52931254eb41c897e8128e2928a94a435a
DOLBIMLED <50545147+Userteck@users.noreply.github.com>
2021-03-09 14:56:28 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 
 const search = document.getElementById('search-books');
 const bookList = document.getElementById('book-list');
-
 console.log(search, bookList);
+var titles = document.getElememtsByClassName('title');
+
+
+console.log(Array.isArray(titles));
+console.log(Array.isArray(Array.from(titles)));
+
+
+titles.forEach(function(item)){
+    console.log(item);
+})
