5543b655bd965311a0e1dc0eed3d30d350eb5ae4
DOLBIMLED <50545147+Userteck@users.noreply.github.com>
2021-03-16 14:10:00 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,35 +1,81 @@
 
-const search = document.getElementById('search-books');
-const bookList = document.getElementById('book-list');
-console.log(search, bookList);
-var titles = document.getElememtsByClassName('titles');
+document.addEventListener('DOMContentLoaded', function(){
 
-
-console.log(Array.isArray(titles));
-console.log(Array.isArray(Array.from(titles)));
-
-
-titles.forEach(function(item){
-    console.log(item);
-})
-
-
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-// console.log(wmf);
-
-var books = document.querySelector('#book-list li .name');
-// console.log(books);
-
-books = documents.querySelectorAll('#book-list li .name');
-// console.log(books);
-
-Array.from(books).forEach(function(book){
-    console.log(book);
-});
-
-
-var books = document.querySelectorAll('#book-list li .name');
-
-Array.from(books).forEach(function(book){
-    book.textContent = 'test';
-});
+    const list = document.querySelector('#book-list ul');
+    const forms = document.forms;
+  
+    // delete books
+    list.addEventListener('click', (e) => {
+      if(e.target.className == 'delete'){
+        const li = e.target.parentElement;
+        li.parentNode.removeChild(li);
+      }
+    });
+  
+    // add books
+    const addForm = forms['add-book'];
+    addForm.addEventListener('submit', function(e){
+      e.preventDefault();
+  
+      // create elements
+      const value = addForm.querySelector('input[type="text"]').value;
+      const li = document.createElement('li');
+      const bookName = document.createElement('span');
+      const deleteBtn = document.createElement('span');
+  
+      // add text content
+      bookName.textContent = value;
+      deleteBtn.textContent = 'delete';
+  
+      // add classes
+      bookName.classList.add('name');
+      deleteBtn.classList.add('delete');
+  
+      // append to DOM
+      li.appendChild(bookName);
+      li.appendChild(deleteBtn);
+      list.appendChild(li);
+    });
+  
+    // hide books
+    const hideBox = document.querySelector('#hide');
+    hideBox.addEventListener('change', function(e){
+      if(hideBox.checked){
+        list.style.display = "none";
+      } else {
+        list.style.display = "initial";
+      }
+    });
+  
+    // filter books
+    const searchBar = forms['search-books'].querySelector('input');
+    searchBar.addEventListener('keyup', (e) => {
+      const term = e.target.value.toLowerCase();
+      const books = list.getElementsByTagName('li');
+      Array.from(books).forEach((book) => {
+        const title = book.firstElementChild.textContent;
+        if(title.toLowerCase().indexOf(e.target.value) != -1){
+          book.style.display = 'block';
+        } else {
+          book.style.display = 'none';
+        }
+      });
+    });
+  
+    // tabbed content
+    const tabs = document.querySelector('.tabs');
+    const panels = document.querySelectorAll('.panel');
+    tabs.addEventListener('click', (e) => {
+      if(e.target.tagName == 'LI'){
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        Array.from(panels).forEach((panel) => {
+          if(panel == targetPanel){
+            panel.classList.add('active');
+          }else{
+            panel.classList.remove('active');
+          }
+        });
+      }
+    });
+  
+  })
